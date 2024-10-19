f8629e0c961552a4f2a5872645e5a291f5e13c67
Boomaa23 <cmo93003@yahoo.com>
2024-10-19 15:03:01 -0700

    Implement cartesia API basic

--- server/app.py
+++ server/app.py
@@ -1,15 +1,17 @@
 import common
+import dotenv
 import flask
 from flask_cors import CORS
 from werkzeug.exceptions import HTTPException
 
-from routes import routes_blueprint
+from routes import api_blueprint
 
 
+dotenv.load_dotenv()
 app = flask.Flask(__name__)
 CORS(app)
 
-app.register_blueprint(routes_blueprint)
+app.register_blueprint(api_blueprint)
 
 
 @app.teardown_appcontext
--- server/routes.py
+++ server/routes.py
@@ -3,6 +3,6 @@ import flask
 api_blueprint = flask.Blueprint('api', __name__)
 
 
-@api_blueprint.route('/api/eggs', methods=['GET'])
+@api_blueprint.route('/api/', methods=['GET'])
 def api_get_eggs():
     return "eggs"
