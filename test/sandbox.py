de0c6b6c7c92e8a5b241889b13aba78d6a79ead3
Zayd Ali <mzali@berkeley.edu>
2024-10-19 18:35:19 -0700

    Added audio parsing for cloning

--- test/sandbox.py
+++ test/sandbox.py
@@ -11,7 +11,6 @@ sys.path.append(server_dir)
 
 from feedback import transcribe_audio, compare_transcriptions, generate_suggestions
 
-CARTESIA_API_KEY = "3ed9262d-677f-4a76-920d-683b6d51ce38"
 CARTESIA_API_KEY_2 = "a1b1697a-174b-4afc-8720-b48feca19d51"
 
 #client = Cartesia(api_key=os.environ.get("CARTESIA_API_KEY"))
@@ -19,16 +18,22 @@ CARTESIA_API_KEY_2 = "a1b1697a-174b-4afc-8720-b48feca19d51"
 def test_clone():
     # Clone a voice using filepath
     url = "https://api.cartesia.ai/voices/clone/clip"
-    files = { "clip": "open('<clone_voice.mp3>', 'rb')" }
-    payload = { "enhance": "true" }
-    headers = {
-        "Cartesia-Version": "2024-06-10",
-        "X-API-Key": CARTESIA_API_KEY_2
-    }
-    response = requests.post(url, data=payload, files=files, headers=headers)
-    print(response.json())
+    
+    # Open the audio file in binary read mode
+    with open('clone_voice.mp3', 'rb') as file:
+        files = { "clip": file }
+        payload = { "enhance": "true" }
+        
+        headers = {
+            "Cartesia-Version": "2024-06-10",
+            "X-API-Key": CARTESIA_API_KEY_2
+        }
+        response = requests.post(url, data=payload, files=files, headers=headers)
+    print(response.status_code)  # To print the status code
+    print(response.json()) 
 
 def test_feedback():
+    # test feedback on improving pronounciation
     audio_kentucky = r"C:\Users\mzayd\Downloads\School\CalHacks11\speak-easy\test\kentucky_testing.wav"
     audio_hindi = r"C:\Users\mzayd\Downloads\School\CalHacks11\speak-easy\test\hindi_testing.wav"
     kentucky_transcribe = transcribe_audio(audio_kentucky)
@@ -38,4 +43,5 @@ def test_feedback():
     print(suggestions)
 
 
-test_feedback()
+#test_feedback()
+test_clone()
