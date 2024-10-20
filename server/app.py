1444260c85d38a71ae1735e8dfce3f8702436f76
Boomaa23 <cmo93003@yahoo.com>
2024-10-19 20:28:53 -0700

    db.db

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ dotenv.load_dotenv()
 app = flask.Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  # In-memory DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'db.db'  # In-memory DB
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
